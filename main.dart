d3986ad22266e2861f65b77251c433fdd813a582
Jamaldin <thisisjoma@gmail.com>
2023-11-27 20:26:46 +0600

    delete

--- main.dart
+++ main.dart
@@ -3,7 +3,6 @@ import 'train.dart';
 
 void main(List<String> args) {
   Engine engine = Engine("tulpar", 120);
-  Car car = Car(fuel: "gas", seats: 2, engine: engine);
   Train train = Train(fuel: "electro", seats: 10, weight: 20);
   train.makeSound();
 }
